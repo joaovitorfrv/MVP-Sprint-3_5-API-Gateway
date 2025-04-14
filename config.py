a68d724880b9c6362444e75c925bb9feb90e2174
João Vitor Frugiuele <joaovitormfrugiuele@gmail.com>
2025-04-13 23:15:39 -0300

    Configs

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-TASK_SERVICE_URL = os.getenv('TASK_SERVICE_URL',"http://localhost:5001")
-BOOKMARK_SERVICE_URL = os.getenv("BOOKMARK_SERVICE_URL","http://localhost:5002")
-EVENT_SERVICE_URL = os.getenv('EVENT_SERVICE_URL',"http://localhost:5003")
+# Endereços dos microserviços dentro da mesma rede Docker
+TASK_SERVICE_URL = os.getenv('TASK_SERVICE_URL', 'http://tasks:5001')
+BOOKMARK_SERVICE_URL = os.getenv('BOOKMARK_SERVICE_URL', 'http://bookmarks:5002')
+EVENT_SERVICE_URL = os.getenv('EVENT_SERVICE_URL', 'http://event:5003')
 
-DEBUG = os.getenv('DEBUG','True') == 'True'
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 PORT = int(os.getenv('PORT', 5000))
